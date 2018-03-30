360e8fcd83765a5b392152f901898d280766b653
timwheelercom <tim@timwheeler.com>
2018-03-30 14:47:26 -0400

    Created mutation.js

---
+++ mutation.js
@@ -0,0 +1,16 @@
+function mutation(arr) {
+
+	var test = arr[1].toLowerCase();
+	var target = arr[0].toLowerCase();
+
+	for(var i = 0; i < test.length; i++){
+
+		if(target.indexOf(test[i]) === -1)
+			return false;
+
+	}
+
+  return true;
+}
+
+mutation(["hey", "hello"]);
