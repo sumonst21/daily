7ad7c0cf79cafaa2c2496ce15755988a80a9ef45
Tim <tim@timwheeler.com>
2018-04-04 21:44:31 -0400

    Created whatIsInAName.js

---
+++ whatIsInAName.js
@@ -0,0 +1,16 @@
+
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+
+  for(i = 0; i < collection.length; i++){
+      if(collection.hasOwnProperty(key))
+        return true;
+    }
+  }
+  // Only change code above this line
+//   return arr;
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
