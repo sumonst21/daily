2fbbfe8313db52c4e8f2060e5f719e32eb167369
Tim <tim@timwheeler.com>
2018-04-08 17:02:05 -0400

    Created getIndexToIns.js

---
+++ getIndexToIns.js
@@ -0,0 +1,14 @@
+function getIndexToIns(arr, num) {
+	
+	arr.sort(function(a,b){
+		return a-b;
+	});
+
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] >= num)
+        	return parseInt(i);
+    }
+	return arr.length;
+}
+
+getIndexToIns([10, 20, 30, 40, 50], 35);
