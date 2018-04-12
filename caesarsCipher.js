e40666c1e246ae53424bb2479dd0f10f680fe22f
Tim Wheeler <tim@timwheeler.com>
2018-04-11 23:08:28 -0400

    Finished caesarsCipher.js

--- caesarsCipher.js
+++ caesarsCipher.js
@@ -1,35 +1,43 @@
 function rot13(str) {
 
-	var splitStr = str.split('');
+	//create an array to store the new values that have been shifted
 	var shifted = [];
+	
+	//create an array to store the end result
 	var result = [];
 
-	for(var i = 0; i < splitStr.length; i++){
+	//loop through str
+	for(var i = 0; i < str.length; i++){
 	
+		//get character codes from str
 		var charCode = str.charCodeAt(i);
-			
-			if(charCode == 32)
-				shifted.push(' ');
-		
-			if(charCode >= 65 && charCode < 78)
-				shifted.push(charCode + 13);
 
-			if(charCode >= 78 && charCode <= 95)
-				shifted.push(charCode - 13);
-				
+        //if char is a space, exclamation, period, or question mark - keep it the same
+        if(charCode == 32 || charCode == 33 || charCode == 46 || charCode == 63)
+            shifted.push(charCode);
+
+        //if char is between letters a-m, shift value forward 13 spaces
+        if(charCode >= 65 && charCode < 78)
+            shifted.push(charCode + 13);
+
+        //if char is between letters n-z, shift value backwards 13 spaces
+        if(charCode >= 78 && charCode <= 95)
+            shifted.push(charCode - 13);
+	
 	}
 
+	//loop over the shifted array
 	for(j = 0; j < shifted.length; j++){
 		
+		//convert char codes to strings
 		shifted[j].toString();
 
+		//push shifted str's to result array
 		result.push(String.fromCharCode(shifted[j]));
-				
+
 	}
 
-	return result.join(' ');
-  	
-}
+	//join result array and return it
+	return result.join('');
 
-// Change the inputs below to test
-rot13("SERR PBQR PNZC");
+}
