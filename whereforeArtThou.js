f4e30565316cecb771e6c2835dc47b7e4a60acfc
Tim Wheeler <tim@timwheeler.com>
2018-04-19 23:05:32 -0400

    Edited whereforeArtThou.js

--- whereforeArtThou.js
+++ whereforeArtThou.js
@@ -1,24 +1,31 @@
 function whatIsInAName(collection, source) {
   // What's in a name?
-  var arr = [];
+var arr = [];
   // Only change code below this line
-  
-	var srcKeys = Object.keys(source);
 
-  	for(var i in collection){
+for(var i in collection){ //loop through objects in the collection array
 
-	var colKeysArr = Object.keys(collection[i]);
-	var colValsArr = Object.values(collection[i]);
+	var decision = true; //set boolean check as true
 
-	var srcKeysArr = Object.keys(source);
-	var srcValsArr = Object.values(source);
-	
-    }
+    var srcKeysArr = Object.keys(source); //define all keys in source
 
-	
+    var srcValsArr = Object.values(source); //define all values in source
 
-	return arr;
+		for(var j in srcKeysArr){  //loop through srcKeysArr
+
+			if(collection[i][srcKeysArr[j]] != source[srcKeysArr[j]]){ //if value in collection does not match value in source
+				
+				decision = false;
+            
+			} 			
+			
+		}
+			if(decision == true){ //if they match 
+				arr.push(collection[i]); //add the collection objects that have matches to arr
+            }
+
 }
 
-      
-whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" })
+  return arr;
+}
+whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 });
