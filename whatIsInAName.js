6269db11d92306e06663a61419bb94b775f73e4f
Tim Wheeler <tim@timwheeler.com>
2018-04-19 22:05:21 -0400

    Edited algorithms

--- whatIsInAName.js
+++ whatIsInAName.js
@@ -1,16 +1,35 @@
-
 function whatIsInAName(collection, source) {
   // What's in a name?
-  var arr = [];
+var arr = [];
   // Only change code below this line
 
-  for(i = 0; i < collection.length; i++){
-      if(collection.hasOwnProperty(key))
-        return true;
-    }
-  }
-  // Only change code above this line
-//   return arr;
+var myArr = [{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }];
+
+
+
+
+for(var i in collection){
+
+	var decision = true;
+
+    var srcKeysArr = Object.keys(source);
+
+    var srcValsArr = Object.values(source);
+
+		for(var j in srcKeysArr){
+
+			if(collection[i][srcKeysArr[j]] != source[srcKeysArr[j]]){
+				
+					decision = false;
+            
+			} 			
+			
+		}
+			if(decision == true){
+				arr.push(collection[i]);
+            }
+
 }
 
-whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+  return arr;
+}
