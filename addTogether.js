7b1b184d9abba18086afc012dd823891c5403bd4
Tim Wheeler <tim@timwheeler.com>
2018-04-26 19:50:17 -0400

    Created addTogether.js

---
+++ addTogether.js
@@ -0,0 +1,26 @@
+function addTogether() {
+
+	var args = arguments;
+
+
+	for(var i in args){
+		if(args[i] != Number)
+			return undefined;
+	
+	if(args.length > 1)
+		return args[0] + args[1]
+	
+	
+
+
+	}
+	
+
+
+
+
+
+ 
+}
+
+addTogether(2)(3);
