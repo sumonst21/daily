0352b7fdfa5df989fabec29dd88e7e838d57bc1b
Tim Wheeler <tim@timwheeler.com>
2018-05-13 23:15:54 -0400

    created smallestCommons.js

--- smallestCommons.js
+++ smallestCommons.js
@@ -1,7 +1,26 @@
-
 function smallestCommons(arr) {
-  return arr;
+  
+var range = arr.sort();
+var array = [];
+
+for(var i = arr[0]; i <= arr[arr.length-1]; i++){
+  array.push(i);
 }
-
-
-smallestCommons([1,5]);
+  
+var x = true;
+var LCM = 0;
+  
+while(x){
+  LCM++;
+  for(var j = array[0]; j <= array[array.length-1]; j++){
+    if(LCM % j !== 0){
+      break;
+    } 
+    else if(j == array[array.length-1]){
+        x = false;
+      }
+    }
+  }
+  return LCM;
+}
+smallestCommons([23, 18]);
