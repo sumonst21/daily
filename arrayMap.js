b518b83caf6ec6af4f8dfce0723ee316932a3d80
Tim Wheeler <tim@timwheeler.com>
2018-08-07 23:52:10 -0400

    added arrayMap.js

---
+++ arrayMap.js
@@ -0,0 +1,7 @@
+var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+const weekends = arr.filter((weekend) => {
+	weekend[0] == 'S';
+});
+
+console.log(weekends);
