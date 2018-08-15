52635bb649b0d61c2354a7aec5184926ba4fb6e9
Tim Wheeler <tim@timwheeler.com>
2018-08-14 22:52:21 -0400

    added chunkyMonkey.js

--- chunkyMonkey.js
+++ chunkyMonkey.js
@@ -1,7 +1,21 @@
-
 function chunkArrayInGroups(arr, size) {
-  // Break it up.
-  return arr;
-}
+    // Break it up.
+  	var res = [];
+	var temp = [];
 
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
+  for (var i in arr) {
+    
+	if (temp.length === size) { // Once temp equals size
+		res.push(temp); // Push temp to res
+		temp = []; // Reset temp
+	}
+	
+	temp.push(arr[i]); // Push arr items to temp
+  }
+  res.push(temp); // Push remaining items to res
+	
+return res;
+}
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4)
+
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) should return [[0, 1, 2, 3], [4, 5]].
